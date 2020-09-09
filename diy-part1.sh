1c40281731f852556966a580237a108c4919ab15
Gzxhwq <Gzxhwq@users.noreply.github.com>
2020-09-09 08:11:30 +0800

    Add OpenClash

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+#Add external packages
+git submodule add https://github.com/vernesong/OpenClash.git package/external-packages/OpenClash
