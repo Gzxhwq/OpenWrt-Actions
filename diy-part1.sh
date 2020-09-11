304797022a3097c8c64549905af3b19331b25336
Gzxhwq <Gzxhwq@users.noreply.github.com>
2020-09-11 14:36:52 +0800

    Add jerrykuku feed source

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,7 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 
 #Add external packages
 git submodule add https://github.com/vernesong/OpenClash.git package/external-packages/OpenClash
