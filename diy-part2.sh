3e90944e2b299a9ba501a7cb0130ee27272e8a36
Gzxhwq <Gzxhwq@users.noreply.github.com>
2020-10-03 08:25:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/10.10.10.100/g' package/base-files/files/bin/config_generate
 
 # Revert to official distfeeds
-sed -i '/ustclug/d' package/lean/default-settings/files/zzz-default-settings
+#sed -i '/ustclug/d' package/lean/default-settings/files/zzz-default-settings
 
 #Apply the patches
 git apply $GITHUB_WORKSPACE/patches/*.patch
