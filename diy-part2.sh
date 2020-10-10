bcb4ec5bbf57ac089972d940fff62206d3ad0870
Gzxhwq <gzxhwq@gmail.com>
2020-10-10 08:12:47 +0800

    Revert "Remove lean's v2ray/v2ray-plugin package"
    This reverts commit 1db6c461b8ae8a6eb155b01c1d2207311c1a9dc1.

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,10 +16,6 @@ sed -i 's/192.168.1.1/10.10.10.100/g' package/base-files/files/bin/config_genera
 # Revert to official distfeeds
 #sed -i '/ustclug/d' package/lean/default-settings/files/zzz-default-settings
 
-# Remove some lean's packages
-rm -r package/lean/v2ray
-rm -r package/lean/v2ray-plugin
-
 #Apply the patches
 git apply $GITHUB_WORKSPACE/patches/*.patch
 
