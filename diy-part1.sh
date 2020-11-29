5cc88d4c3758bc34ac91ef7a667d71e9dee6d75f
Gzxhwq <Gzxhwq@users.noreply.github.com>
2020-11-29 12:14:17 +0800

    passwall: change feed address

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 
 #Add external packages
