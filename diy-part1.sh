1673cdea765993944753651a10466fd41b83ad41
Gzxhwq <gzxhwq@gmail.com>
2021-02-03 22:38:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,9 +18,8 @@ sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds
 sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 
 #Add external packages
-git submodule add https://github.com/vernesong/OpenClash.git package/external-packages/OpenClash
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/external-packages/luci-app-openclash
 git submodule add https://github.com/destan19/OpenAppFilter.git package/external-packages/OpenAppFilter
-git submodule add https://github.com/rufengsuixing/luci-app-adguardhome.git package/external-packages/luci-app-adguardhome
 git submodule add https://github.com/tty228/luci-app-serverchan.git package/external-packages/luci-app-serverchan
 git submodule add -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/external-packages/luci-theme-edge
 git submodule add https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/external-packages/luci-theme-infinityfreedom
