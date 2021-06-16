b1877c6fffd33aa635a709adcd5d1671f97563cf
Gzxhwq <Gzxhwq@users.noreply.github.com>
2021-06-16 13:09:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,9 +11,10 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
+sed -i '$a src-git ssrp https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 
@@ -25,3 +26,4 @@ git submodule add -b 18.06 https://github.com/garypang13/luci-theme-edge.git pac
 git submodule add https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/external-packages/luci-theme-infinityfreedom
 git submodule add https://github.com/garypang13/luci-app-eqos.git package/external-packages/luci-app-eqos
 git submodule add https://github.com/lisaac/luci-app-dockerman.git package/external-packages/luci-app-dockerman
+ 
