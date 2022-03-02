4f13052cf10fcb84d03a36cf870c5019fdd0dd14
Gzxhwq <gzxhwq@gmail.com>
2022-03-02 15:40:52 +0800

    Change IP list source

--- n1-diy-part2.sh
+++ n1-diy-part2.sh
@@ -32,14 +32,14 @@ sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generat
 git apply $GITHUB_WORKSPACE/patches/*.patch
 
 # Update mwan3helper's IP pools
-#wget https://ispip.clang.cn/all_cn_cidr.txt -O feeds/luci/applications/luci-app-mwan3helper/root/etc/mwan3helper/all_cn.txt
-#wget https://ispip.clang.cn/chinatelecom_cidr.txt -O feeds/luci/applications/luci-app-mwan3helper/root/etc/mwan3helper/chinatelecom.txt
-#wget https://ispip.clang.cn/unicom_cnc_cidr.txt -O feeds/luci/applications/luci-app-mwan3helper/root/etc/mwan3helper/unicom_cnc.txt
-#wget https://ispip.clang.cn/cmcc_cidr.txt -O feeds/luci/applications/luci-app-mwan3helper/root/etc/mwan3helper/cmcc.txt
-#wget https://ispip.clang.cn/crtc_cidr.txt -O feeds/luci/applications/luci-app-mwan3helper/root/etc/mwan3helper/crtc.txt
-#wget https://ispip.clang.cn/cernet_cidr.txt -O feeds/luci/applications/luci-app-mwan3helper/root/etc/mwan3helper/cernet.txt
-#wget https://ispip.clang.cn/gwbn_cidr.txt -O feeds/luci/applications/luci-app-mwan3helper/root/etc/mwan3helper/gwbn.txt
-#wget https://ispip.clang.cn/othernet_cidr.txt -O feeds/luci/applications/luci-app-mwan3helper/root/etc/mwan3helper/othernet.txt
+wget https://github.com/misakaio/chnroutes2/raw/master/chnroutes.txt -O feeds/luci/applications/luci-app-mwan3helper/root/etc/mwan3helper/all_cn.txt
+wget https://github.com/gaoyifan/china-operator-ip/raw/ip-lists/chinanet.txt -O feeds/luci/applications/luci-app-mwan3helper/root/etc/mwan3helper/chinatelecom.txt
+wget https://github.com/gaoyifan/china-operator-ip/raw/ip-lists/unicom.txt -O feeds/luci/applications/luci-app-mwan3helper/root/etc/mwan3helper/unicom_cnc.txt
+wget https://github.com/gaoyifan/china-operator-ip/raw/ip-lists/cmcc.txt -O feeds/luci/applications/luci-app-mwan3helper/root/etc/mwan3helper/cmcc.txt
+wget https://github.com/gaoyifan/china-operator-ip/raw/ip-lists/tietong.txt -O feeds/luci/applications/luci-app-mwan3helper/root/etc/mwan3helper/crtc.txt
+wget https://github.com/gaoyifan/china-operator-ip/raw/ip-lists/cernet.txt -O feeds/luci/applications/luci-app-mwan3helper/root/etc/mwan3helper/cernet.txt
+wget https://github.com/gaoyifan/china-operator-ip/raw/ip-lists/drpeng.txt -O feeds/luci/applications/luci-app-mwan3helper/root/etc/mwan3helper/gwbn.txt
+wget https://github.com/gaoyifan/china-operator-ip/raw/ip-lists/cstnet.txt -O feeds/luci/applications/luci-app-mwan3helper/root/etc/mwan3helper/othernet.txt
 
 # Change dnsproxy behave
 #sed -i 's/--cache --cache-min-ttl=3600/--cache --cache-min-ttl=600/g' ./feeds/luci/applications/luci-app-turboacc/root/etc/init.d/turboacc
