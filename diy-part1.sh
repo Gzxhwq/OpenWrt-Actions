d13bb1233909b85feba3b310cbd4dc10a439ef1e
Gzxhwq <gzxhwq@gmail.com>
2022-03-16 13:02:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,12 +14,12 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '1i src-git mosdns https://github.com/QiuSimons/openwrt-mos' feeds.conf.default
 sed -i '$a src-git ssrp https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall;packages' feeds.conf.default
 sed -i '$a src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall;luci' feeds.conf.default
 sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
 sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+sed -i '1i src-git mosdns https://github.com/QiuSimons/openwrt-mos' feeds.conf.default
 
 #Add external packages
 #svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/others/luci-app-openclash
