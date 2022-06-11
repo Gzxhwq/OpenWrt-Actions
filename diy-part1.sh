9ef2f8d3062cf94317cc3c593528eb1c30693f2e
Gzxhwq <gzxhwq@gmail.com>
2022-06-11 23:39:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,11 +15,12 @@
 
 # Add a feed source
 sed -i '$a src-git ssrp https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall;packages' feeds.conf.default
-#sed -i '$a src-git passwall_luci https://github.com/Gzxhwq/openwrt-passwall;private' feeds.conf.default
-sed -i '$a src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall;luci' feeds.conf.default
+sed -i '$a src-git passwall_packages https://github.com/Gzxhwq/openwrt-passwall;packages-me' feeds.conf.default
+#sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall;packages' feeds.conf.default
+sed -i '$a src-git passwall_luci https://github.com/Gzxhwq/openwrt-passwall;luci-me' feeds.conf.default
+#sed -i '$a src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall;luci' feeds.conf.default
 #sed -i '$a src-git passwall2 https://github.com/Gzxhwq/openwrt-passwall2;private' feeds.conf.default
-sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
+#sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
 sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 #sed -i '1i src-git mosdns https://github.com/QiuSimons/openwrt-mos' feeds.conf.default
 
