4377ee6ff25638451e1ce55b2c1d2a34c4b4119d
Gzxhwq <gzxhwq@gmail.com>
2022-11-05 13:21:33 +0800

    add passwall2

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,7 +19,7 @@ sed -i '$a src-git passwall_packages https://github.com/Gzxhwq/openwrt-passwall;
 #sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall;packages' feeds.conf.default
 #sed -i '$a src-git passwall_luci https://github.com/Gzxhwq/openwrt-passwall;luci-me' feeds.conf.default
 sed -i '$a src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall;luci' feeds.conf.default
-#sed -i '$a src-git passwall2 https://github.com/Gzxhwq/openwrt-passwall2;private' feeds.conf.default
+sed -i '$a src-git passwall2 https://github.com/Gzxhwq/openwrt-passwall2;private' feeds.conf.default
 #sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
 sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 #sed -i '1i src-git mosdns https://github.com/QiuSimons/openwrt-mos' feeds.conf.default
