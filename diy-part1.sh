516a74f292204c3bbe3bd49e886436d8ac907195
Gzxhwq <gzxhwq@gmail.com>
2022-11-18 23:59:20 +0800

    update workflow

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,8 +15,8 @@
 
 # Add a feed source
 #sed -i '$a src-git ssrp https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git passwall_packages https://github.com/Gzxhwq/openwrt-passwall;packages-me' feeds.conf.default
-#sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall;packages' feeds.conf.default
+#sed -i '$a src-git passwall_packages https://github.com/Gzxhwq/openwrt-passwall;packages-me' feeds.conf.default
+sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall;packages' feeds.conf.default
 #sed -i '$a src-git passwall_luci https://github.com/Gzxhwq/openwrt-passwall;luci-me' feeds.conf.default
 sed -i '$a src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall;luci' feeds.conf.default
 sed -i '$a src-git passwall2 https://github.com/Gzxhwq/openwrt-passwall2;private' feeds.conf.default
@@ -35,3 +35,4 @@ sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.co
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/others/luci-app-amlogic
 git clone https://github.com/thinktip/luci-theme-neobird.git package/others/luci-theme-neobird
 svn co https://github.com/QiuSimons/openwrt-mos/trunk/v2ray-geodata package/others/v2ray-geodata
+svn co https://github.com/Gzxhwq/openwrt-passwall/branches/packages-me/sing-box package/others/sing-box
