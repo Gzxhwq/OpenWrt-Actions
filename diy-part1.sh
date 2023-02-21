5e3a458efcf39730a25eafad6f1224d2b853de99
Gzxhwq <gzxhwq@gmail.com>
2023-02-21 17:47:14 +0800

    become reality

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,8 +18,8 @@
 #sed -i '$a src-git passwall_packages https://github.com/Gzxhwq/openwrt-passwall;packages-me' feeds.conf.default
 sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall;packages' feeds.conf.default
 #sed -i '$a src-git passwall_luci https://github.com/Gzxhwq/openwrt-passwall;luci-me' feeds.conf.default
-sed -i '$a src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall;luci' feeds.conf.default
-sed -i '$a src-git passwall2 https://github.com/Gzxhwq/openwrt-passwall2;private' feeds.conf.default
+sed -i '$a src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall;luci-reality' feeds.conf.default
+sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
 #sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
 sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 #sed -i '1i src-git mosdns https://github.com/QiuSimons/openwrt-mos' feeds.conf.default
@@ -35,4 +35,6 @@ sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.co
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/others/luci-app-amlogic
 git clone https://github.com/thinktip/luci-theme-neobird.git package/others/luci-theme-neobird
 svn co https://github.com/QiuSimons/openwrt-mos/trunk/v2ray-geodata package/others/v2ray-geodata
-svn co https://github.com/Gzxhwq/openwrt-passwall/branches/packages-me/sing-box package/others/sing-box
+
+svn co https://github.com/Gzxhwq/openwrt-passwall/branches/sing-box-git/sing-box package/others/sing-box
+svn co https://github.com/Gzxhwq/openwrt-passwall/branches/xray-core-git/xray-core package/others/xray-core
