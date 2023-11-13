afe90b2950b29e130d7e40e2d6f37e9d83bc9678
Gzxhwq <Gzxhwq@users.noreply.github.com>
2023-11-14 00:16:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,18 +28,18 @@ sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' fee
 #Add external packages
 #svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/others/luci-app-openclash
 #git submodule add https://github.com/destan19/OpenAppFilter.git package/others/OpenAppFilter
-#git submodule add https://github.com/tty228/luci-app-serverchan.git package/others/luci-app-serverchan
-#git submodule add -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/others/luci-theme-edge
-#git submodule add https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/others/luci-theme-infinityfreedom
+git submodule add https://github.com/jerrykuku/luci-app-argon-config.git package/others/luci-app-argon-config
+git submodule add -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/others/luci-theme-argon
+git submodule add https://github.com/ophub/luci-app-amlogic.git package/others/luci-app-amlogic
 #git submodule add https://github.com/garypang13/luci-app-eqos.git package/others/luci-app-eqos
 #git submodule add https://github.com/lisaac/luci-app-dockerman.git package/others/luci-app-dockerman
 #git clone https://github.com/thinktip/luci-theme-neobird.git package/others/luci-theme-neobird
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/others/luci-app-amlogic
+# svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/others/luci-app-amlogic
 
-svn co https://github.com/jerrykuku/luci-theme-argon/branches/18.06/ package/others/luci-theme-argon
-svn co https://github.com/jerrykuku/luci-app-argon-config/trunk/ package/others/luci-app-argon-config
+# svn co https://github.com/jerrykuku/luci-theme-argon/branches/18.06/ package/others/luci-theme-argon
+# svn co https://github.com/jerrykuku/luci-app-argon-config/trunk/ package/others/luci-app-argon-config
 
-svn co https://github.com/Gzxhwq/openwrt-passwall-packages/branches/sing-box-git/sing-box package/others/sing-box
+# svn co https://github.com/Gzxhwq/openwrt-passwall-packages/branches/sing-box-git/sing-box package/others/sing-box
 # svn co https://github.com/Gzxhwq/openwrt-passwall-packages/branches/xray-core-git/xray-core package/others/xray-core
 
 #svn co https://github.com/Gzxhwq/openwrt-package/trunk/tailscale package/others/tailscale
